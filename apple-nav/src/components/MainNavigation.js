be65a8a71254650c06427b61e1ce85c0ee7379cf
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-29 07:50:05 +0800

    Add logo

--- apple-nav/src/components/MainNavigation.js
+++ apple-nav/src/components/MainNavigation.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 import NavigationContainer from './NavigationStyles'
-// import SubNavigation from './NavigationStyles'
+import Logo from './StyleComponents/Logo'
 
 const MainNavigation = props => {
   const { data } = props
   return (
     <NavigationContainer>
+      <Logo>
+        <NavLink to="/"> 
+          <img src="https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_large.svg" alt="Apple Logo" />
+        </NavLink>
+      </Logo>
       {data.map(category => (
         <NavLink 
           key={category.name} 
