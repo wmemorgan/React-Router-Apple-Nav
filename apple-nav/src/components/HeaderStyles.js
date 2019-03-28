4199d75256af7fca0a70013b1316cd034f251ffd
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-28 11:12:49 +0800

    Add HeaderContainer style settings

--- apple-nav/src/components/HeaderStyles.js
+++ apple-nav/src/components/HeaderStyles.js
@@ -1,2 +1,11 @@
 import styled from 'styled-components'
-import { color, flex } from './StyleComponents/theme'
+import { color, colorScheme, flex } from './StyleComponents/theme'
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  max-width: 1200px;
+  ${flex('row','center','space-evenly')}
+  color: ${colorScheme.headerFontColor};
+  background: ${colorScheme.headerBgColor};
+  padding: 10px;
+`
