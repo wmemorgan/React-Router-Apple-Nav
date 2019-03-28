6e036579831d8cb1290aafe5ea58978a904cd2cf
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-28 12:40:52 +0800

    Add state and load data
    Incorporate Header component

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 import AppContainer from './components/StyleComponents/AppStyles'
+import Header from './components/Header'
 
+import menuData from './data'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({data: menuData})
+  }
+
   render() {
     return (
       <AppContainer>
-
+        <Header data={this.state.data}/>
       </AppContainer>
     )
   }
--- apple-nav/src/components/StyleComponents/AppStyles.js
+++ apple-nav/src/components/StyleComponents/AppStyles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { flex } from './theme'
 
-export const AppContainer = styled.div`
+const AppContainer = styled.div`
   width: 100%;
   max-width: 1440px;
   ${flex('column','center','center')}
